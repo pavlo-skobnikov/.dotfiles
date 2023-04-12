6fa9ddc5f0de176cad0b7c927f0a6af43315d599
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-04-12 16:19:04 +0300

    Add zsh configurations

---
+++ .config/zsh/exports.zsh
@@ -0,0 +1,14 @@
+#!/bin/sh
+HISTSIZE=1000000
+SAVEHIST=1000000
+export PATH="$HOME/.local/bin":$PATH
+export MANPAGER='nvim +Man!'
+export MANWIDTH=999
+export EDITOR='nvim'
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+export JAVA_HOME=$HOME/.sdkman/candidates/java/current
+export M2_HOME=$HOME/.sdkman/candidates/maven/current
+export PATH=$HOME/.cargo/bin:$PATH
+export PATH="$HOME/.local/share/neovim/bin":$PATH
+export PATH="$HOME/.emacs.d/bin":$PATH
+export PATH="$HOME/.local/share/nvim/mason/bin":$PATH
