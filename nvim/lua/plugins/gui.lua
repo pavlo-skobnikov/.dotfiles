08f09b6e342254f1e8bdd39c1fe389b71bdb7184
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-04-23 23:24:03 +0300

    Update lualine inactive buffers and remove neotree

--- nvim/lua/plugins/gui.lua
+++ nvim/lua/plugins/gui.lua
@@ -6,7 +6,7 @@ return {
     name = 'catppuccin',
     config = function()
       require('catppuccin').setup {
-        flavour = 'frappe', -- latte, frappe, macchiato, mocha
+        flavour = 'frappe', -- Options: latte, frappe, macchiato, mocha
       }
 
       vim.cmd [[ colorscheme catppuccin ]]
@@ -32,57 +32,8 @@ return {
               end,
             },
           },
-        },
-      }
-    end,
-  },
-  {
-    -- File Explorer
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- Not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-    },
-    config = function()
-      -- Remove legacy key bindings
-      vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
-
-      -- Special symbols for diagnostics
-      vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
-      vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
-      vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
-      vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
-
-      -- Key bindings
-      vim.keymap.set('n', '<leader>e', ':Neotree<CR>', { noremap = true, desc = 'Open Explorer' })
-      vim.keymap.set(
-        'n',
-        '<leader>E',
-        ':NeoTreeReveal<CR>',
-        { noremap = true, desc = 'Open Current File in Explorer' }
-      )
-      vim.keymap.set(
-        'n',
-        '<leader>te',
-        ':NeoTreeShowToggle<CR>',
-        { noremap = true, desc = 'Toggle File Explorer' }
-      )
-
-      -- Enable NeoTree
-      require('neo-tree').setup {
-        default_component_configs = {
-          git_status = {
-            symbols = {
-              -- Custom git status type symbols
-              untracked = '?', -- Had to change this from "" to "?" because it was causing issues
-              ignored = '',
-              unstaged = '',
-              staged = '',
-              conflict = '',
-            },
-          },
+          lualine_b = { 'filename' },
+          lualine_c = {},
         },
       }
     end,
