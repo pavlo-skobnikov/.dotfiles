3f0645d8fb58f6f4ad8c798da4161a8ede3cb042
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-05-29 19:38:33 +0300

    nvim: Fix up `formatting.lua`

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -12,9 +12,25 @@ return {
 
       null_ls.setup {
         sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.google_java_format,
-          null_ls.builtins.formatting.prettier,
+          -- Diagnostic
+          null_ls.builtins.diagnostics.pylint.with {
+            -- Disable displaying diagnostics visually
+            diagnostic_config = { underline = false, virtual_text = false, signs = false },
+            method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+          },                                              -- "python"
+          null_ls.builtins.diagnostics.markdownlint,      -- "markdown"
+          -- Formatting
+          null_ls.builtins.formatting.sql_formatter,      -- "sql"
+          null_ls.builtins.formatting.jq,                 -- "json"
+          null_ls.builtins.formatting.stylua,             -- "lua", "luau"
+          null_ls.builtins.formatting.google_java_format, -- "java"
+          null_ls.builtins.formatting.black,              -- "python"
+          null_ls.builtins.formatting.prettier,           -- "javascript", "javascriptreact",
+          -- "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json",
+          -- "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars"
+
+          -- Diagnostic + Formatting
+          null_ls.builtins.diagnostics.ktlint, -- "kotlin"
         },
       }
     end,
