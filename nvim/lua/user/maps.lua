370c41b1b7f2bab176df8c27e0b73a1a36a39305
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-07-01 00:44:36 +0300

    nvim: Simplify and reduce basic remaps

--- nvim/lua/user/maps.lua
+++ nvim/lua/user/maps.lua
@@ -1,16 +1,7 @@
-local function options(description)
-  if description == nil then
-    return { silent = true, noremap = false }
-  else
-    return { silent = true, noremap = false, desc = description }
-  end
-end
-
 -- Shorten key mapping function name
 local map = vim.keymap.set
 
 --Remap space as leader key
--- map("", "<Space>", "<Nop>", options())
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ';'
 
@@ -22,48 +13,22 @@ vim.g.maplocalleader = ';'
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Normal
--- Executing `x` on texts throws it into the void register
-map('n', 'x', '"_x', options())
-map('n', 'x', '"_x', options())
-
 -- Remove highlights on <ESC>
-map('n', '<ESC>', ':noh<CR>', options())
+map('n', '<ESC>', ':noh<CR>', { silent = true, noremap = true })
 
--- Better window navigation
-map('n', '<C-h>', '<C-w>h', options())
-map('n', '<C-j>', '<C-w>j', options())
-map('n', '<C-k>', '<C-w>k', options())
-map('n', '<C-l>', '<C-w>l', options())
-
--- Resize with arrows
-vim.cmd [[ nnoremap <A-=> :res -2<CR> ]]
-vim.cmd [[ nnoremap <A--> :res +2<CR> ]]
-vim.cmd [[ nnoremap <A-.> :vertical res -2<CR> ]]
-vim.cmd [[ nnoremap <A-,> :vertical res +2<CR> ]]
-
--- Easy window navigation
-for i = 1, 9 do
-  local left_hand_side = '<leader>' .. i
-  local right_hand_side = i .. '<C-w>w'
-
-  map('n', left_hand_side, right_hand_side, options('Move to window ' .. i))
-end
-
+-- Open Chrome for selected link
 local function get_visual_selection()
   -- Yank current visual selection into the 'v' register
-  --
   -- NOTE: that this makes no effort to preserve this register
   vim.cmd 'noau normal! "vy"'
 
   return vim.fn.getreg 'v'
 end
 
--- Open Chrome for selected link
 map('v', '<leader>o', function()
   local selection = get_visual_selection()
 
   local command = "!open '/Applications/Google Chrome.app' '" .. selection .. "'"
 
   vim.cmd(command)
-end, options 'Open link in Chrome')
+end, { silent = true, noremap = false, description = 'Open Selected Link' })
