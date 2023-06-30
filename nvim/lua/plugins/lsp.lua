ca94fde23804ce90036d72e6e95f93b02ed231fb
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-07-01 00:36:04 +0300

    nvim: Update LSP & DAP mappings + add LSs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -142,16 +142,16 @@ return {
           end, { desc = 'Toggle DAP UI' })
 
           -- nvim-dap mappings
-          vim.keymap.set('n', '<leader>dac', function()
+          vim.keymap.set('n', '<leader>dc', function()
             require('dap').continue()
           end, { desc = 'Continue' })
-          vim.keymap.set('n', '<leader>dan', function()
+          vim.keymap.set('n', '<leader>dsn', function()
             require('dap').step_over()
-          end, { desc = 'Step Over' })
-          vim.keymap.set('n', '<leader>dai', function()
+          end, { desc = 'Step Over (Next)' })
+          vim.keymap.set('n', '<leader>dsi', function()
             require('dap').step_into()
           end, { desc = 'Step Into' })
-          vim.keymap.set('n', '<leader>dao', function()
+          vim.keymap.set('n', '<leader>dso', function()
             require('dap').step_out()
           end, { desc = 'Step Out' })
           vim.keymap.set('n', '<leader>dbt', function()
@@ -163,23 +163,23 @@ return {
           vim.keymap.set('n', '<leader>dbl', function()
             require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
           end, { desc = 'Set Log Point' })
-          vim.keymap.set('n', '<leader>dr', function()
+          vim.keymap.set('n', '<leader>do', function()
             require('dap').repl.open()
           end, { desc = 'Open REPL' })
-          vim.keymap.set('n', '<leader>dR', function()
+          vim.keymap.set('n', '<leader>dr', function()
             require('dap').run_last()
           end, { desc = 'Run Last' })
-          vim.keymap.set({ 'n', 'v' }, '<leader>dh', function()
+          vim.keymap.set({ 'n', 'v' }, '<leader>dwh', function()
             require('dap.ui.widgets').hover()
           end, { desc = 'Hover' })
-          vim.keymap.set({ 'n', 'v' }, '<leader>dp', function()
+          vim.keymap.set({ 'n', 'v' }, '<leader>dwp', function()
             require('dap.ui.widgets').preview()
           end, { desc = 'Preview' })
-          vim.keymap.set('n', '<leader>dF', function()
+          vim.keymap.set('n', '<leader>dwf', function()
             local widgets = require 'dap.ui.widgets'
             widgets.centered_float(widgets.frames)
           end, { desc = 'Frames' })
-          vim.keymap.set('n', '<leader>ds', function()
+          vim.keymap.set('n', '<leader>dws', function()
             local widgets = require 'dap.ui.widgets'
             widgets.centered_float(widgets.scopes)
           end, { desc = 'Scopes' })
@@ -188,7 +188,7 @@ return {
           vim.keymap.set('n', '<leader>dfc', function()
             require('telescope').extensions.dap.commands {}
           end, { desc = 'Find Commands' })
-          vim.keymap.set('n', '<leader>dfC', function()
+          vim.keymap.set('n', '<leader>dfg', function()
             require('telescope').extensions.dap.configurations {}
           end, { desc = 'Find Configurations' })
           vim.keymap.set('n', '<leader>dfb', function()
@@ -280,24 +280,25 @@ return {
 
       -- Add some additional language servers
       lsp.ensure_installed {
-        'jdtls', -- Java
-        'groovyls', -- Groovy
+        'jdtls',                  -- Java
         'kotlin_language_server', -- Kotlin
-        'clojure_lsp', -- Clojure
-        'gradle_ls', -- Gradle
-        'tsserver', -- JavaScript/TypeScript
-        'rust_analyzer', -- Rust
-        'pylsp', -- Python
-        'zls', -- Zig
-        'gopls', -- Go
-        'lua_ls', -- Lua
+        'clojure_lsp',            -- Clojure
+        'gradle_ls',              -- Gradle
+        'groovyls',               -- Groovy
+        'gopls',                  -- Go
+        'pylsp',                  -- Python
+        'lua_ls',                 -- Lua
         'fennel_language_server', -- Fennel
-        'bashls', -- Bash
-        'marksman', -- Markdown
-        'dockerls', -- Dockerfile
-        'sqlls', -- SQL
-        'yamlls', -- YAML
-        'jsonls', -- JSON
+        'tsserver',               -- JavaScript/TypeScript
+        'zls',                    -- Zig
+        'clangd',                 -- C
+        'rust_analyzer',          -- Rust
+        'bashls',                 -- Bash
+        'marksman',               -- Markdown
+        'dockerls',               -- Dockerfile
+        'sqlls',                  -- SQL
+        'yamlls',                 -- YAML
+        'jsonls',                 -- JSON
       }
 
       -- JDTLS will be set up via the nvim-jdtls plugin
@@ -330,21 +331,24 @@ return {
         local telescope_builtin = require 'telescope.builtin'
 
         local function create_opts(description)
-          return { buffer = bufnr, remap = false, desc = description }
+          return {
+            silent = true,
+            buffer = bufnr,
+            remap = false,
+            desc = description,
+          }
         end
 
         -- LSP Mappings try to mostly be tied to `g` as the leader keybinding
-        vim.keymap.set('n', 'K', function()
+        vim.keymap.set({ 'n', 'v' }, 'K', function()
           vim.lsp.buf.hover()
         end, create_opts 'Show Hover Info')
-        vim.keymap.set('i', '<C-S-k>', function()
+        vim.keymap.set({ 'n', 'i' }, '<C-S-k>', function()
           vim.lsp.buf.signature_help()
         end, create_opts 'Show Signature Help')
 
         vim.keymap.set('n', '<leader>=', function()
           vim.lsp.buf.format { async = true }
-
-          vim.cmd 'w'
         end, create_opts 'Format Buffer')
 
         vim.keymap.set('n', ']d', function()
@@ -376,38 +380,38 @@ return {
           telescope_builtin.lsp_type_definitions()
         end, create_opts 'Go to Type Definition')
 
-        vim.keymap.set('n', 'gs', function()
+        vim.keymap.set('n', '<leader>csd', function()
           telescope_builtin.lsp_document_symbols()
         end, create_opts 'Search Document Symbols')
-        vim.keymap.set('n', 'gS', function()
+        vim.keymap.set('n', '<leader>csw', function()
           telescope_builtin.lsp_workspace_symbols()
         end, create_opts 'Search Workspace Symbols')
 
-        vim.keymap.set('n', 'gl', function()
+        vim.keymap.set('n', '<leader>cd', function()
           vim.diagnostic.open_float()
         end, create_opts 'Open Diagnostics Float')
-        vim.keymap.set('n', 'gL', function()
+        vim.keymap.set('n', '<leader>cl', function()
           vim.lsp.codelens.run()
         end, create_opts 'Run Code Lens')
 
-        vim.keymap.set('n', 'ga', function()
+        vim.keymap.set('n', '<leader>ca', function()
           vim.lsp.buf.code_action()
         end, create_opts 'Open Code Actions')
 
-        vim.keymap.set('n', 'gr', function()
+        vim.keymap.set('n', '<leader>cr', function()
           telescope_builtin.lsp_references()
         end, create_opts 'Find References')
-        vim.keymap.set('n', 'gR', function()
+        vim.keymap.set('n', '<leader>cn', function()
           vim.lsp.buf.rename()
         end, create_opts 'Rename Symbol')
 
-        vim.keymap.set('n', 'gQ', function()
+        vim.keymap.set('n', '<leader>cdd', function()
           telescope_builtin.diagnostics { bufnr = 0 }
         end, create_opts 'Show Document Diagnostics')
-        vim.keymap.set('n', 'gW', function()
+        vim.keymap.set('n', '<leader>cdw', function()
           telescope_builtin.diagnostics { severity = vim.diagnostic.severity.WARN }
         end, create_opts 'Show Workspace Warnings')
-        vim.keymap.set('n', 'gE', function()
+        vim.keymap.set('n', '<leader>cde', function()
           telescope_builtin.diagnostics { severity = vim.diagnostic.severity.ERROR }
         end, create_opts 'Show Workspace Errors')
       end)
@@ -436,7 +440,7 @@ return {
         -- For cmp-dap
         enabled = function()
           return vim.api.nvim_buf_get_option(0, 'buftype') ~= 'prompt'
-            or require('cmp_dap').is_dap_buffer()
+              or require('cmp_dap').is_dap_buffer()
         end,
         -- Enable snippets
         snippet = {
@@ -463,7 +467,7 @@ return {
           { name = 'nvim_lsp' },
           { name = 'conjure' },
           { name = 'luasnip', keyword_length = 2 },
-          { name = 'buffer', keyword_length = 4 },
+          { name = 'buffer',  keyword_length = 4 },
         },
       }
     end,
