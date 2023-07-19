1c6392a7123407deb1e3a756f0566c80e8996ed4
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-07-19 23:45:15 +0300

    nvim: Leave only most used langs in `formatting`

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -12,29 +12,11 @@ return {
 
       null_ls.setup {
         sources = {
-          -- Diagnostic
-          null_ls.builtins.diagnostics.pylint.with {
-            -- Disable displaying diagnostics visually
-            diagnostic_config = { underline = false, virtual_text = false, signs = false },
-            method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-          }, -- "python"
-          null_ls.builtins.diagnostics.markdownlint, -- "markdown"
           -- Formatting
           null_ls.builtins.formatting.sql_formatter, -- "sql"
           null_ls.builtins.formatting.jq, -- "json"
           null_ls.builtins.formatting.stylua, -- "lua", "luau"
           null_ls.builtins.formatting.google_java_format, -- "java"
-          null_ls.builtins.formatting.zprint, -- "clojure"
-          null_ls.builtins.formatting.zigfmt, -- "zig"
-          null_ls.builtins.formatting.gofmt, -- "go"
-          null_ls.builtins.formatting.black, -- "python"
-          null_ls.builtins.formatting.prettier, -- "javascript", "javascriptreact",
-          -- "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json",
-          -- "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars"
-
-          -- Diagnostic + Formatting
-          null_ls.builtins.diagnostics.ktlint, -- "kotlin"
-          null_ls.builtins.formatting.ktlint,
         },
       }
     end,
