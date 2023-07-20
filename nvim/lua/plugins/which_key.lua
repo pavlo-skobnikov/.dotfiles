db0d2716acbeb20008db32e71e4929616687329c
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-07-20 12:49:57 +0300

    nvim: Finalize `which_key`

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -1,28 +1,34 @@
--- Description: Plugin for dynamically showing key mappings
+-- Plugin for dynamically showing key mappings
 return {
-  {
-    -- The popup menu plugin
-    'folke/which-key.nvim',
-    config = function()
-      -- Set a delay for the popup to...well...pop up
-      vim.o.timeout = true
-      vim.o.timeoutlen = 200
+    {
+        -- The popup menu plugin
+        'folke/which-key.nvim',
+        keys = { '<leader>', '<localleader>' },
+        config = function()
+            -- Set a delay for the popup to...well...pop up
+            vim.o.timeout = true
+            vim.o.timeoutlen = 200
 
-      local which_key = require 'which-key'
+            local which_key = require 'which-key'
 
-      which_key.setup {
-        hidden = { "<leader>'" },
-      }
-
-      -- Used only for setting groups
-      which_key.register({
-        f = { name = 'find' },
-        t = { name = 'toggle' },
-        h = { name = 'hunks' },
-        g = { name = 'git' },
-        r = { name = 'replace' },
-        w = { name = 'workspace' },
-      }, { prefix = '<leader>' })
-    end,
-  },
+            -- Used only for setting groups
+            which_key.register({
+                c = {
+                    name = 'code',
+                    s = { name = 'search' },
+                },
+                d = {
+                    name = 'dap',
+                    b = { name = 'breakpoints' },
+                    f = { name = 'find' },
+                    s = { name = 'step' },
+                    w = { name = 'widget' },
+                },
+                f = { name = 'find' },
+                t = { name = 'toggle' },
+                h = { name = 'hunks' },
+                g = { name = 'git' },
+            }, { prefix = '<leader>' })
+        end,
+    },
 }
