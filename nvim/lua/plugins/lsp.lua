9c3f53b2983520920cc976b84577eeb440eab16e
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-08-12 21:07:43 +0300

    hammerspoon: the beginnings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -68,7 +68,10 @@ return {
                         settings = {
                             Lua = {
                                 diagnostics = {
-                                    globals = { 'vim' },
+                                    globals = {
+                                        'vim',
+                                        'hs',
+                                    },
                                 },
                             },
                         },
