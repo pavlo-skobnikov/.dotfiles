aca6d62c6da3a238a60f687daeb096108515aaaa
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-10-18 14:59:02 +0300

    nvim: `oil` show hidden default

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -11,7 +11,11 @@ return {
         'stevearc/oil.nvim', -- Vim buffer-like editing for the file system
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-            require('oil').setup()
+            require('oil').setup {
+                view_options = {
+                    show_hidden = true,
+                },
+            }
 
             vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
         end,
