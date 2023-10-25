ea553441f1f0fdcfd605dbd142ca81e1f04fa1a5
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-10-25 08:58:07 +0300

    hammerspoon: slim down config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -18,28 +18,17 @@ end
 
 local launchOrFocusToBindMap = {
     a = 'Activity Monitor', -- [a]ctivity
-    b = '',
     c = 'Calendar', -- [c]alendar
     d = 'Docker Desktop', -- [d]ocker
-    e = 'System Preferences', -- `e` for `environment`
-    f = 'Finder', -- [f]inder
     g = 'Google Chrome', -- [g]oogle
     i = 'IntelliJ IDEA', -- [i]ntellij
-    j = '',
     k = 'Kitty', -- [k]itty
-    -- l = '', -> Reserved for reloading Hammerspoon's configuration
     m = 'Mail', -- [m]ail
     n = 'NordPass', -- [n]ordpass
     o = 'Obsidian', -- [o]bsidian
-    p = 'Postman', -- [p]ostman
-    -- q = '', -> Reserved for closing applications
     r = 'Reminders', -- [r]eminders
     s = 'Slack', -- [s]lack
     t = 'Telegram', -- [t]elegram
-    u = '',
-    v = '',
-    w = 'Microsoft Teams', -- microsoft (upside down m -> [w])
-    -- x = '', -> Reserved for relaunching Amethyst
     y = 'Youtube Music', -- [y]outube
     z = 'zoom.us', -- [z]oom
 }
