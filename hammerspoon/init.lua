364af0a745b6c8a0b76263f93a82c00153f6acfc
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-11-08 10:57:15 +0200

    hammerspoon: add comments for hyper bindings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -17,23 +17,23 @@ local function bindLaunchOrFocusAppToHyperAndKey(appName, key)
 end
 
 local launchOrFocusToBindMap = {
-    e = 'Google Chrome',
-    r = 'Youtube Music',
-    t = 'Telegram',
+    e = 'Google Chrome', -- N[e]twork
+    r = 'Youtube Music', -- [R]ecords
+    t = 'Telegram', -- [T]elegram
 
-    s = 'Calendar',
-    d = 'Mail',
-    f = 'Reminders',
-    g = 'Slack',
+    s = 'Calendar', -- [S]chedule
+    d = 'Mail', -- Correspon[d]ence
+    f = 'Reminders', -- E[f]fort
+    g = 'Slack', -- [G]roup chat
 
-    c = 'Activity Monitor',
-    v = 'Docker Desktop',
-    b = 'zoom.us',
+    c = 'Activity Monitor', -- [C]PU & RAM
+    v = 'Docker Desktop', -- [V]Ms
+    b = 'zoom.us', -- [B]usiness meetings
 
-    u = 'Kitty',
-    i = 'IntelliJ IDEA',
-    o = 'Obsidian',
-    p = 'NordPass',
+    u = 'Kitty', -- CLI [U]tils
+    i = 'IntelliJ IDEA', -- [I]ntellij
+    o = 'Obsidian', -- [O]bsidian
+    p = 'NordPass', -- [P]asswords
 }
 
 for key, appName in pairs(launchOrFocusToBindMap) do
