6d2fefd6b8d910d4b50b6cd171fbe150c48170d6
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-11-08 12:04:55 +0200

    tmux: remove own session switcher script

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,11 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dev/projects ~/dev/projects/personal ~/dev/projects/personal -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find \
+        ~/dev/projects \
+        ~/dev/projects/personal \
+        ~/dev/projects/personal \
+        -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
@@ -18,7 +22,7 @@ if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
+if ! tmux has-session -t=$selected_name 2>/dev/null; then
     tmux new-session -ds $selected_name -c $selected
 fi
 
